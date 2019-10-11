49969ebc2a1120142e208f07b23c5213158192f3
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-10-10 21:48:41 -0500

    Followed video 2, modified app to use Jinja templates & css/html formatting

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html', pageTitle = 'My Calculator')
 
 @app.route('/Eric')
 def eric():
-    return "Hello, it's Eric"
+    return render_template('eric.html', pageTitle = 'Erics page')
 
 
 if __name__ == '__main__':
