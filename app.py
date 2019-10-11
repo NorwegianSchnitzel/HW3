423ebd25a101700ec9dd544cb4876d13486b4d29
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-10-10 23:13:41 -0500

    Attempted to format output to no avail, leaving in current form until lab tomorrow

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ def index():
     return render_template('index.html', display="", pageTitle = 'Loan Calculator')
 
 @app.route('/calculate', methods=['GET', 'POST'])
-def add():
+def loan():
     if request.method == 'POST':
         form = request.form
         loan = float(form['loanAmt'])
         payments = int(form['payments'])
         interest = float(form['interest'])
         discountFactor = (((1 + interest)**payments) - 1) / (interest*(1 + interest)**payments)
-        loanPayment = loan/discountFactor
+        loanPayment = round(loan/discountFactor, 2)
         return render_template('index.html', display=loanPayment, pageTitle='Loan Calculator')
         
     return redirect("/")
