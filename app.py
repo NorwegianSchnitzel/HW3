541065d4f13fe14540c01ef2e0528c73c989e31f
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-10-10 22:58:39 -0500

    Fixed bugs in Feature branch, app is operational, merging back into master

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', display="", pageTitle = 'Loan Calculator')
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/calculate', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         form = request.form
         loan = float(form['loanAmt'])
         payments = int(form['payments'])
         interest = float(form['interest'])
-        discountFactor = (((1 + interest)**payments) - 1) / (interest(1 + interest)**payments)
+        discountFactor = (((1 + interest)**payments) - 1) / (interest*(1 + interest)**payments)
         loanPayment = loan/discountFactor
         return render_template('index.html', display=loanPayment, pageTitle='Loan Calculator')
         
