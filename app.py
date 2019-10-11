c378450b10444ce0310c92b4773ca874a596cbf5
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-10-10 22:35:46 -0500

    Formatted index.html, getting error upon calculation

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle = 'My Calculator')
+    return render_template('index.html', display="", pageTitle = 'Loan Calculator')
 
-@app.route('/Eric')
-def eric():
-    return render_template('eric.html', pageTitle = 'Erics page')
-
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        form = request.form
+        loan = float(form['loanAmt'])
+        payments = int(form['payments'])
+        interest = float(form['interest'])
+        discountFactor = (((1 + interest)**payments) - 1) / (interest(1 + interest)**payments)
+        loanPayment = loan/discountFactor
+        return render_template('index.html', display=loanPayment, pageTitle='Loan Calculator')
+        
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
