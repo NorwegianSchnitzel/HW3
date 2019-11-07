94b3ceda6eb1a948affad8a0ecd61458b085906c
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-11-06 21:40:00 -0600

    Got add_animal working

--- app.py
+++ app.py
@@ -5,20 +5,21 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'SuperSecretKey'
 
-class AnimalsForm(FlaskForm):
+app.config['SECRET_KEY'] = 'SuperSecretKey'
+
+class AnimalForm(FlaskForm):
     Name = StringField('Animal Name:', validators=[DataRequired()])
     Country = StringField('Country:', validators = [DataRequired()])
     
 
 @app.route('/')
 def index():
-    return render_template('index.html', display="", pageTitle = 'Eric\'s Animals')
+    return render_template('base.html', pageTitle = 'Eric\'s Animals')
 
 @app.route('/add_animal', methods =['GET', 'POST'])
 def add_animal():
-    form = AnimalsForm()
+    form = AnimalForm()
     if form.validate_on_submit():
         return "<h2> The animal is {0} from {1}".format(form.Name.data, form.Country.data)
     
