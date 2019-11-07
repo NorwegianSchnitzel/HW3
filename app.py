2673218f24e96b9af3940ab3459fc11ea655f9f3
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-11-07 10:53:40 -0600

    Displaying table on index page, functionality to add an animal to database and redirect back to index

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import secrets
+
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'SuperSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = conn
+db = SQLAlchemy(app)
+
+
+class ewestby_animalsapp(db.Model):
+    animalID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(255))
+    Country = db.Column(db.String(255))
+    Weight = db.Column(db.Integer)
+    Quantity = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return "id: {0} | Name: {1} | Country: {2} | Weight: {3} | Quantity: {4}".format(self.id, self.Name, self.Country, self.Weight, self.Quantity)
+
 
 class AnimalForm(FlaskForm):
     Name = StringField('Animal Name:', validators=[DataRequired()])
     Country = StringField('Country:', validators = [DataRequired()])
+    Weight = IntegerField('Weight:', validators = [DataRequired()])
+    Quantity = IntegerField('Quantity:', validators = [DataRequired()])
     
 
 @app.route('/')
 def index():
-    return render_template('base.html', pageTitle = 'Eric\'s Animals')
+    all_animals = ewestby_animalsapp.query.all()
+    return render_template('index.html', animals = all_animals, pageTitle = 'Eric\'s Animals')
 
 @app.route('/add_animal', methods =['GET', 'POST'])
 def add_animal():
     form = AnimalForm()
     if form.validate_on_submit():
-        return "<h2> The animal is {0} from {1}".format(form.Name.data, form.Country.data)
+        animal = ewestby_animalsapp(Name = form.Name.data, Country = form.Country.data, Weight = form.Weight.data, Quantity = form.Quantity.data)
+        db.session.add(animal)
+        db.session.commit()
+        return redirect('/')
     
     return render_template('add_animal.html', form=form, pageTitle='Add a New Animal')
 
